6fc7a4ff98112cccde7467497f9c3fe4243bf436
Harshad0018 <harshadpawar0018@gmail.com>
2022-11-11 12:49:48 +0530

    First Commit Done

---
+++ JenkinProject/src/Jenkin2/JenkinClass.java
@@ -0,0 +1,10 @@
+package Jenkin2;
+
+public class JenkinClass {
+
+	public static void main(String[] args) {
+	System.out.println("Hellow");
+
+	}
+
+}
---
+++ JenkinProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Harsh
+ *
+ */
+module JenkinProject {
+}
